0533eacae035ce6868fea02e7318e9e32e6683c5
Lucas FRESIERS <fresiers.lucas@gmail.com>
2019-12-17 14:10:43 +0100

    init project

---
+++ src/main/java/com/epsi/gestionBancaire/App.java
@@ -0,0 +1,11 @@
+package com.epsi.gestionBancaire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
