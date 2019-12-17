17bd3001e0a5f14dff527a00520463df825a6755
Louis DOUBLECOURT <louis.dbl@hotmail.fr>
2019-12-17 14:27:03 +0100

    createFolder

--- infra-as-code/createFolder.groovy
+++ infra-as-code/createFolder.groovy
@@ -2,17 +2,5 @@ String name = "GestionBancaire"
 folder(name) {
     description 'Voici le dossier contenant tous les jobs du projet Gestion bancaire'
 }
-listView(name) {
-columns {
-buildButton()
-disableProject(false)
-lastBuildConsole()
-lastDuration()
-lastFailure()
-lastSuccess()
-name()
-progressBar()
-releaseButton()
-status()
-}
-}
+
+
